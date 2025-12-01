c25e48593bd48fccbb83bd4cc3edd2510dc6de61
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 01:06:23 -0300

    feat: adição do arquivo IPessoaRepository.cs

---
+++ developments/GerenciamentoDePessoas-4/GerenciamentoDePessoas/Repository/IPessoaRepository.cs
@@ -0,0 +1,14 @@
+﻿using GerenciamentoDePessoas.Models;
+
+namespace GerenciamentoDePessoas.Repository
+{
+    public interface IPessoaRepository
+    {
+       Task<List<Pessoa>> BuscarTodos();
+        Task<bool> VerificarSePessoaExiste(string cpf);
+        Task<Pessoa> Criar(Pessoa pessoa);
+        Task<Pessoa> BuscarPorId(int id);
+        Task<Pessoa> Editar(Pessoa pessoa);
+        Task Apagar(Pessoa pessoa);
+    }
+}
