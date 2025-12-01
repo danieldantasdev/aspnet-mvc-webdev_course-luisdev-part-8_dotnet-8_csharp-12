1434e276697680419798a45b1f6baa1f68ab0c9d
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 01:06:28 -0300

    feat: adição do arquivo site.js

---
+++ developments/GerenciamentoDePessoas-4/GerenciamentoDePessoas/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('.tabela-pessoas').DataTable(
+        {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/2.2.2/i18n/pt-BR.json',
+            }
+        });
+});
